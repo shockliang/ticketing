64da20669bf19bc6a6b0b4c39aade2dff849b689
shockliang <shockwater@gmail.com>
2020-10-07 00:06:10 +0800

    Converting errors to response.

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,12 +1,25 @@
 import {Request, Response, NextFunction} from "express";
+import {RequestValidationError} from "../errors/request-validation-error";
+import {DatabaseConnectionError} from "../errors/database-connection-error";
 
 export const errorHandler = (
     err: Error,
     req: Request,
     res: Response,
     next: NextFunction) => {
-    console.log('Something wrong', err);
+
+    if (err instanceof RequestValidationError) {
+        const formattedErrors = err.errors.map(error => {
+            return {message: error.msg, field: error.param}
+        });
+        return res.status(400).send({errors: formattedErrors});
+    }
+
+    if (err instanceof DatabaseConnectionError) {
+        return res.status(500).send({errors: [{message: err.reason}]});
+    }
+
     res.status(400).send({
-        message: err.message
+        errors: [{message: err.message}]
     })
-};
+}
