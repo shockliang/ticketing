c4dbea1b8ea968e6ba6b819a63a67c9002703191
shockliang <shockwater@gmail.com>
2020-10-14 00:39:54 +0800

    Generating a jwt.

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -3,6 +3,7 @@ import {body, validationResult} from 'express-validator';
 import {User} from "../models/user";
 import {RequestValidationError} from "../errors/request-validation-error";
 import {BadRequestError} from "../errors/bad-request-error";
+import jwt from 'jsonwebtoken';
 
 const router = express.Router();
 
@@ -31,6 +32,15 @@ router.post('/api/users/signup',[
 
     const user = User.build({email, password});
     await user.save();
+
+    const userJwt = jwt.sign({
+        id: user.id,
+        email: user.email
+    }, 'asdfadfaf');
+
+    req.session = {
+        jwt: userJwt
+    };
 
     res.status(201).send(user);
 });
