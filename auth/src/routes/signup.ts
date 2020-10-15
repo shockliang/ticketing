503b37dcb788b28b60ac3d6e2516f5a354337486
shockliang <shockwater@gmail.com>
2020-10-15 20:14:45 +0800

    Accessing evn variables in a pod.

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -36,7 +36,7 @@ router.post('/api/users/signup',[
     const userJwt = jwt.sign({
         id: user.id,
         email: user.email
-    }, 'asdfadfaf');
+    }, process.env.JWT_KEY!);
 
     req.session = {
         jwt: userJwt
