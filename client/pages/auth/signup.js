308396280de2c91ca66de9d449a1b2d66131b245
shockliang <shockwater@gmail.com>
2020-11-10 00:05:42 +0800

    Fetching data during SSR.

--- client/pages/auth/signup.js
+++ client/pages/auth/signup.js
@@ -11,7 +11,7 @@ export default () => {
     body: {
       email, password
     },
-    onSuccess: () => Router.push('/');
+    onSuccess: () => Router.push('/')
   });
 
   const onSubmit = async (event) => {
--- client/pages/index.js
+++ client/pages/index.js
@@ -1,3 +1,25 @@
-export default () => {
+import axios from 'axios';
+
+const LandingPage =  ({currentUser}) => {
+  console.log(currentUser);
+
   return <h1>Loading Page</h1>;
 };
+
+LandingPage.getInitialProps = async () => {
+  if (typeof window === 'undefined') {
+    const {data} = await axios.get(
+      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
+        headers: {
+          Host: 'ticketing.dev'
+        }
+      }
+    );
+    return data;
+  } else {
+    const {data} = await axios.get('/api/users/currentuser');
+    return data;
+  }
+}
+
+export default LandingPage;
