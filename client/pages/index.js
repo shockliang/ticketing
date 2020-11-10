fdbd5a884e11d0992963a85fd08907fd9514c5e6
shockliang <shockwater@gmail.com>
2020-11-11 00:27:34 +0800

    Pasing through the cookie.

--- client/pages/index.js
+++ client/pages/index.js
@@ -6,13 +6,11 @@ const LandingPage =  ({currentUser}) => {
   return <h1>Loading Page</h1>;
 };
 
-LandingPage.getInitialProps = async () => {
+LandingPage.getInitialProps = async ({req}) => {
   if (typeof window === 'undefined') {
     const {data} = await axios.get(
       'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
-        headers: {
-          Host: 'ticketing.dev'
-        }
+        headers: req.headers
       }
     );
     return data;
