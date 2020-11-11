325246f4fbddf47e3b9b9d55c11f208c90b184af
shockliang <shockwater@gmail.com>
2020-11-12 00:22:50 +0800

    A reusable api client.

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import buildClient from '../api/build-client';
 
 const LandingPage =  ({currentUser}) => {
   console.log(currentUser);
@@ -6,18 +6,11 @@ const LandingPage =  ({currentUser}) => {
   return <h1>Loading Page</h1>;
 };
 
-LandingPage.getInitialProps = async ({req}) => {
-  if (typeof window === 'undefined') {
-    const {data} = await axios.get(
-      'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser', {
-        headers: req.headers
-      }
-    );
-    return data;
-  } else {
-    const {data} = await axios.get('/api/users/currentuser');
-    return data;
-  }
+LandingPage.getInitialProps = async (context) => {
+  const client = buildClient(context);
+  const {data} = await client.get('/api/users/currentuser');
+
+  return data;
 }
 
 export default LandingPage;
