1fe769dbd70bdd6927cd224ef302845d3c094d5c
shockliang <shockwater@gmail.com>
2020-11-15 14:24:21 +0800

    Implementing reusable header and moving getInitialProps.

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,25 @@
 import 'bootstrap/dist/css/bootstrap.css';
+import buildClient from "../api/build-client";
 
-export default ({Component, pageProps}) => {
-  return <Component {...pageProps} />;
+const AppComponent =  ({Component, pageProps}) => {
+  return (
+    <div>
+      <h1>Header</h1>
+      <Component {...pageProps} />
+    </div>
+  );
 };
+
+AppComponent.getInitialProps = async (appContent) => {
+  const client = buildClient(appContent.ctx);
+  const {data} = await client.get('/api/users/currentuser');
+
+  let pageProps = {};
+  if(appContent.Component.getInitialProps) {
+    pageProps = await appContent.Component.getInitialProps(appContent.ctx);
+  }
+
+  return data;
+}
+
+export default AppComponent;
