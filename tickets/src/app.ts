e130effda8f43c630d6f1f599387aa6a0e673f4c
shockliang <shockwater@gmail.com>
2020-11-30 01:32:54 +0800

    Creating the new router.

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -2,6 +2,9 @@ import express from 'express';
 import 'express-async-errors';
 import {json} from 'body-parser';
 import cookieSession from 'cookie-session';
+import {errorHandler, NotFoundError} from '@shockticketing/common'
+
+import {createTicketRouter} from "./routes/new";
 
 const app = express();
 app.set('trust proxy', true);
@@ -10,6 +13,8 @@ app.use(cookieSession({
     signed: false,
     secure: process.env.NODE_ENV !== 'test',
 }));
+
+app.use(createTicketRouter);
 
 app.all('*', async (req, res) => {
     throw new NotFoundError()
