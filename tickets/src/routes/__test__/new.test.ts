dc376285ee2a1d7f06a9c614e9c9a8c3e30c4f99
shockliang <shockwater@gmail.com>
2020-12-02 00:02:19 +0800

    Faking authentication during tests.

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -16,6 +16,15 @@ it('can only be accessed if the user is signed in', async () => {
         .expect(401);
 });
 
+it('return a status other than 401 if the user is singed in', async () => {
+    const response = await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({});
+
+    expect(response.status).not.toEqual(401);
+});
+
 it('returns an error if an invalid title is provided', async() => {
 
 });
