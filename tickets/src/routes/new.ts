c135ca39c42b4b350dd1fb87d59145161b3f2686
shockliang <shockwater@gmail.com>
2020-12-04 00:01:19 +0800

    Creation via route handler.

--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,6 +1,7 @@
 import express, {Response, Request} from "express";
 import {body} from 'express-validator';
 import {requireAuth, validateRequest} from "@shockticketing/common";
+import {Ticket} from "../models/ticket";
 
 const router = express.Router();
 
@@ -11,8 +12,17 @@ router.post(
         body('price').isFloat({gt: 0}).withMessage('Price must be great than 0')
     ],
     validateRequest,
-    (req: Request, res:Response) => {
-    res.sendStatus(200);
+    async (req: Request, res:Response) => {
+        const {title, price} = req.body;
+
+        const ticket = Ticket.build({
+            title,
+            price,
+            userId: req.currentUser!.id
+        });
+        await ticket.save();
+
+        res.status(201).send(ticket);
 });
 
 export {router as createTicketRouter};
