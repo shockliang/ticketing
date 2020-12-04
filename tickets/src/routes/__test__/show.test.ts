fda7d48c0cdbedfe49c913153f5f077fd52c6ceb
shockliang <shockwater@gmail.com>
2020-12-05 00:46:40 +0800

    Test show route.

---
+++ tickets/src/routes/__test__/show.test.ts
@@ -0,0 +1,31 @@
+import request from 'supertest';
+import {app} from "../../app";
+
+it('return 404 if the ticket is not found', async () => {
+    await request(app)
+        .get('/api/tickets/adfafafaafe3')
+        .send()
+        .expect(404);
+});
+
+it('return a ticket if the ticket is found', async() => {
+    const title = 'my ticket';
+    const price = 10;
+
+    const response = await request(app)
+        .post('/api/tickets')
+        .set('Cookie', global.signin())
+        .send({
+            title,
+            price
+        })
+        .expect(201);
+
+    const ticketResponse = await request(app)
+        .get(`/api/tickets/${response.body.id}`)
+        .send()
+        .expect(200);
+
+    expect(ticketResponse.body.title).toEqual(title);
+    expect(ticketResponse.body.price).toEqual(price);
+});
