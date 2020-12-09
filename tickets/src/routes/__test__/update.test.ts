0cc8b8f27fe764632075e26a477a43981a8e698e
shockliang <shockwater@gmail.com>
2020-12-10 00:58:56 +0800

    Final update changes.

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -45,9 +45,58 @@ it('return a 401 if the user does not own the ticket', async () => {
 });
 
 it('return a 400 if the user provides an invalid title or price', async () => {
+    const cookie = global.signin();
 
+    const response = await request(app)
+        .post('/api/tickets')
+        .set('Cookie', cookie)
+        .send({
+            title: 'my ticket',
+            price: 20
+        });
+
+    await request(app)
+        .put(`/api/tickets/${response.body.id}`)
+        .set('Cookie', cookie)
+        .send({
+            title: '',
+            price: 20
+        })
+        .expect(400);
+
+    await request(app)
+        .put(`/api/tickets/${response.body.id}`)
+        .set('Cookie', cookie)
+        .send({
+            title: 'my ticket',
+            price: -1
+        })
+        .expect(400);
 });
 
 it('updates the ticket provided valid inputs', async () => {
+    const cookie = global.signin();
 
+    const response = await request(app)
+        .post('/api/tickets')
+        .set('Cookie', cookie)
+        .send({
+            title: 'my ticket',
+            price: 20
+        });
+    await request(app)
+        .put(`/api/tickets/${response.body.id}`)
+        .set('Cookie', cookie)
+        .send({
+            title: 'my new ticket',
+            price: 25
+        })
+        .expect(200);
+
+    const ticketResponse = await request(app)
+        .get(`/api/tickets/${response.body.id}`)
+        .send();
+
+    expect(ticketResponse.body.title).toEqual('my new ticket');
+    expect(ticketResponse.body.price).toEqual(25);
 });
--- tickets/src/routes/update.ts
+++ tickets/src/routes/update.ts
@@ -1,4 +1,4 @@
-import express, {Response,Request} from "express";
+import express, {Response, Request} from "express";
 import {body} from 'express-validator';
 import {
     validateRequest,
@@ -10,18 +10,38 @@ import {Ticket} from "../models/ticket";
 
 const router = express.Router();
 
-router.put('/api/tickets/:id', requireAuth, async (req:Request, res: Response) => {
-    const ticket = await Ticket.findById(req.params.id);
+router.put(
+    '/api/tickets/:id',
+    requireAuth,
+    [
+        body('title')
+            .not()
+            .isEmpty()
+            .withMessage('Title is required'),
+        body('price')
+            .isFloat({gt: 0})
+            .withMessage('Price must be provided and must be greater than 0')
+    ],
+    validateRequest,
+    async (req: Request, res: Response) => {
+        const ticket = await Ticket.findById(req.params.id);
 
-    if (!ticket) {
-        throw new NotFoundError();
-    }
+        if (!ticket) {
+            throw new NotFoundError();
+        }
 
-    if (ticket.userId !== req.currentUser!.id) {
-        throw new NotAuthorizedError();
-    }
+        if (ticket.userId !== req.currentUser!.id) {
+            throw new NotAuthorizedError();
+        }
 
-    res.send(ticket);
-});
+        ticket.set({
+            title: req.body.title,
+            price: req.body.price
+        });
+
+        await ticket.save();
+
+        res.send(ticket);
+    });
 
 export {router as updateTicketRouter};
