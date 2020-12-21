1b7b35fbbe50221ab10df63887b2341269110dee
shockliang <shockwater@gmail.com>
2020-12-22 00:37:48 +0800

    Implementing abstract publisher.

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,4 +1,5 @@
 import nats from 'node-nats-streaming';
+import {TicketCreatedPublisher} from "../events/ticket-created-publisher";
 
 console.clear();
 
@@ -9,13 +10,10 @@ const stan = nats.connect('ticketing', 'abc', {
 stan.on('connect', () => {
     console.log('Publisher connected to NATS');
 
-    const data = JSON.stringify({
+    const publisher = new TicketCreatedPublisher(stan);
+    publisher.publish({
         id: '123',
-        title: 'my title',
-        price: 20
+        title: 'MY TICKET',
+        price: 25
     });
-
-    stan.publish('ticket:created', data, () => {
-        console.log('Event published');
-    })
 });
