1a5d034cc171df1c9442b0143ad9db0887364b3b
shockliang <shockwater@gmail.com>
2020-12-23 00:09:29 +0800

    Awaiting event publication.

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -7,13 +7,17 @@ const stan = nats.connect('ticketing', 'abc', {
     url: 'http://localhost:4222',
 });
 
-stan.on('connect', () => {
+stan.on('connect', async () => {
     console.log('Publisher connected to NATS');
 
     const publisher = new TicketCreatedPublisher(stan);
-    publisher.publish({
-        id: '123',
-        title: 'MY TICKET',
-        price: 25
-    });
+    try {
+        await publisher.publish({
+            id: '123',
+            title: 'MY TICKET',
+            price: 25
+        });
+    } catch (err) {
+        console.log(err);
+    }
 });
