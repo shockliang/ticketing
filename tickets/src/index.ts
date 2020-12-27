5f61eb9bb8e1c401786d010a87d6bca4a1e10d93
shockliang <shockwater@gmail.com>
2020-12-28 00:13:02 +0800

    Graceful shutdown.

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -3,15 +3,22 @@ import {app} from "./app";
 import {natsWrapper} from "./nats-wrapper";
 
 const start = async () => {
-    if(!process.env.JWT_KEY) {
+    if (!process.env.JWT_KEY) {
         throw new Error('JWT_KEY must be defined');
     }
-    if(!process.env.MONGO_URI) {
+    if (!process.env.MONGO_URI) {
         throw new Error('MONGO_URI must be defined');
     }
 
     try {
-        await natsWrapper.connect('ticketing', 'fdfjll', 'http://nats-service:4222')
+        await natsWrapper.connect('ticketing', 'fdfjll', 'http://nats-service:4222');
+        natsWrapper.client.on('close', () => {
+            console.log('NATS connection closed!');
+            process.exit();
+        });
+        process.on('SIGINT', () => natsWrapper.client.close());
+        process.on('SIGTERM', () => natsWrapper.client.close());
+
         await mongoose.connect(process.env.MONGO_URI, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
