d87f32839896cecbfda7b57f0a575bb14b377157
shockliang <shockwater@gmail.com>
2021-01-13 02:05:19 +0800

    Asserting reserved tickets.

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -1,6 +1,9 @@
 import mongoose from 'mongoose';
 import request from 'supertest';
 import {app} from "../../app";
+import {Order} from "../../models/order";
+import {Ticket} from "../../models/ticket";
+import {OrderStatus} from "@shockticketing/common";
 
 it('returns an error if the ticket does not exist', async () => {
     const ticketId = mongoose.Types.ObjectId();
@@ -15,7 +18,25 @@ it('returns an error if the ticket does not exist', async () => {
 });
 
 it('returns an error if the ticket is already reserved', async () => {
+    const ticket = Ticket.build({
+        title: 'My title',
+        price: 25
+    });
+    await ticket.save();
 
+    const order = Order.build({
+        ticket,
+        userId: 'myiddddddddddddddddd',
+        status: OrderStatus.Created,
+        expiresAt: new Date()
+    });
+    await order.save();
+
+    await request(app)
+        .post('/api/orders')
+        .set('Cookie', global.signin())
+        .send({ticketId: ticket.id})
+        .expect(400);
 });
 
 it('reserves a ticket', async () => {
