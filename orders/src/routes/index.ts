b7778c0ae68a6eefd3d48333dafc1571bb05e59d
shockliang <shockwater@gmail.com>
2021-01-16 20:10:10 +0800

    Fetching orders for particular user test.

--- orders/src/routes/index.ts
+++ orders/src/routes/index.ts
@@ -5,7 +5,7 @@ import {Order} from "../models/order";
 const router = express.Router();
 
 router.get('/api/orders', requireAuth,  async(req: Request, res: Response) => {
-    const orders = Order.find({
+    const orders = await Order.find({
         userId: req.currentUser!.id
     }).populate('ticket');
 
