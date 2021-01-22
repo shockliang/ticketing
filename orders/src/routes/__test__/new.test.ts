7ee0b129127b5cb63632792d198d849c57fe31a7
shockliang <shockwater@gmail.com>
2021-01-22 23:16:39 +0800

    Testing event publish for new router handler.

--- orders/src/routes/__test__/new.test.ts
+++ orders/src/routes/__test__/new.test.ts
@@ -4,6 +4,7 @@ import {app} from "../../app";
 import {Order} from "../../models/order";
 import {Ticket} from "../../models/ticket";
 import {OrderStatus} from "@shockticketing/common";
+import {natsWrapper} from '../../nats-wrapper';
 
 it('returns an error if the ticket does not exist', async () => {
     const ticketId = mongoose.Types.ObjectId();
@@ -53,4 +54,18 @@ it('reserves a ticket', async () => {
         .expect(201);
 });
 
-it.todo('emits an order created event');
+it('emits an order created event', async () => {
+    const ticket = Ticket.build({
+        title: 'MyTicket',
+        price: 25
+    });
+    await ticket.save();
+
+    await request(app)
+        .post('/api/orders')
+        .set('Cookie', global.signin())
+        .send({ticketId: ticket.id})
+        .expect(201);
+
+    expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
