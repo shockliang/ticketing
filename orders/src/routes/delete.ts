292d1c14359dde5ac177a2bf26fdd5566a3a56ac
shockliang <shockwater@gmail.com>
2021-01-23 00:03:36 +0800

    Publishing order cancellation.

--- orders/src/routes/delete.ts
+++ orders/src/routes/delete.ts
@@ -1,12 +1,14 @@
 import express, {Request, Response} from "express";
 import {NotAuthorizedError, NotFoundError, requireAuth} from "@shockticketing/common";
 import {Order, OrderStatus} from "../models/order";
+import {OrderCancelledPublisher} from "../events/publishers/order-cancelled-publisher";
+import {natsWrapper} from "../nats-wrapper";
 
 const router = express.Router();
 
 router.delete('/api/orders/:orderId', requireAuth, async (req: Request, res: Response) => {
     const {orderId} = req.params;
-    const order = await Order.findById(orderId);
+    const order = await Order.findById(orderId).populate('ticket');
     if (!order) {
         throw new NotFoundError();
     }
@@ -16,6 +18,13 @@ router.delete('/api/orders/:orderId', requireAuth, async (req: Request, res: Res
 
     order.status = OrderStatus.Cancelled;
     await order.save();
+
+    await new OrderCancelledPublisher(natsWrapper.client).publish({
+        id: order.id,
+        ticket: {
+            id: order.ticket.id
+        }
+    });
 
     res.status(204).send(order);
 });
