9578b1d370a6dee4c8620dacce4b5b01013b0291
shockliang <shockwater@gmail.com>
2021-01-28 20:07:18 +0800

    Adding more tests for ticket model.

--- tickets/src/models/__test__/ticket.test.ts
+++ tickets/src/models/__test__/ticket.test.ts
@@ -26,3 +26,19 @@ it('implements optimistic concurrency control', async (done) => {
 
     throw new Error('Should not reach this point');
 });
+
+it('increments the version number on multiple saves', async () => {
+    const ticket = Ticket.build({
+        title: '#1',
+        price: 20,
+        userId: '1234'
+    });
+    await ticket.save();
+    expect(ticket.version).toEqual(0);
+
+    await ticket.save();
+    expect(ticket.version).toEqual(1);
+
+    await ticket.save();
+    expect(ticket.version).toEqual(2);
+});
