87d1b93ba2bfeefd4a2fadd8eb3449489bfd1f08
shockliang <shockwater@gmail.com>
2021-02-13 00:51:29 +0800

    Rejecting edits of reserved tickets.

--- tickets/src/routes/__test__/update.test.ts
+++ tickets/src/routes/__test__/update.test.ts
@@ -2,6 +2,7 @@ import request from 'supertest';
 import {app} from "../../app";
 import mogoose from 'mongoose';
 import {natsWrapper} from "../../nats-wrapper";
+import {Ticket} from "../../models/ticket";
 
 it('return a 404 if the provided id does not exit', async () => {
     const id = mogoose.Types.ObjectId().toHexString();
@@ -122,4 +123,29 @@ it('publishes an event', async () => {
         .expect(200);
 
     expect(natsWrapper.client.publish).toHaveBeenCalled();
+});
+
+it('reject updates if the ticket is reserved', async () => {
+    const cookie = global.signin();
+
+    const response = await request(app)
+        .post('/api/tickets')
+        .set('Cookie', cookie)
+        .send({
+            title: 'my ticket',
+            price: 20
+        });
+
+    const ticket = await Ticket.findById(response.body.id);
+    ticket!.set({orderId: mogoose.Types.ObjectId().toHexString()});
+    await ticket!.save();
+
+    await request(app)
+        .put(`/api/tickets/${response.body.id}`)
+        .set('Cookie', cookie)
+        .send({
+            title: 'my new ticket',
+            price: 25
+        })
+        .expect(400);
 });
