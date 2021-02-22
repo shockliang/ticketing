00c3f1dc4f38aa19fd913068d28a835b965c6cf7
shockliang <shockwater@gmail.com>
2021-02-23 02:59:24 +0800

    Implementing expiration complete listener.

---
+++ orders/src/events/listeners/expiration-complete-listener.ts
@@ -0,0 +1,31 @@
+import {ExpirationCompleteEvent, Listener, Subjects, OrderStatus} from "@shockticketing/common";
+import {queueGroupName} from "./queue-group-name";
+import {Message} from 'node-nats-streaming';
+import {Order} from "../../models/order";
+import {OrderCancelledPublisher} from "../publishers/order-cancelled-publisher";
+
+export class ExpirationCompleteListener extends Listener<ExpirationCompleteEvent> {
+    readonly subject = Subjects.ExpirationComplete;
+    queueGroupName = queueGroupName;
+
+    async onMessage(data: ExpirationCompleteEvent["data"], msg: Message) {
+        const order = await Order.findById(data.orderId);
+
+        if (!order) {
+            throw new Error('Order not found')
+        }
+
+        order.set({
+            status: OrderStatus.Cancelled,
+        });
+        await order.save();
+
+        await new OrderCancelledPublisher(this.client).publish({
+            id: order.id,
+            version: order.version,
+            ticket: {
+                id: order.ticket.id
+            }
+        });
+    }
+}
