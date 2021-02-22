5c743ebf196fbefb7b1f692bc87fb9e6a5f9f455
shockliang <shockwater@gmail.com>
2021-02-22 21:41:23 +0800

    Publishing an event on job processing.

---
+++ expiration/src/events/publishers/expiration-complete-publisher.ts
@@ -0,0 +1,6 @@
+import {Subjects, Publisher, ExpirationCompleteEvent} from "@shockticketing/common";
+
+export class ExpirationCompletePublisher extends Publisher<ExpirationCompleteEvent> {
+    readonly subject = Subjects.ExpirationComplete;
+
+}
