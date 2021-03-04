5cf3d01950a254153a1e234201c89b1b89e7457a
shockliang <shockwater@gmail.com>
2021-03-05 00:33:46 +0800

    Implementing new router for payments.

--- payments/src/app.ts
+++ payments/src/app.ts
@@ -2,7 +2,8 @@ import express from 'express';
 import 'express-async-errors';
 import {json} from 'body-parser';
 import cookieSession from 'cookie-session';
-import {errorHandler, NotFoundError, currentUser} from '@shockticketing/common'
+import {errorHandler, NotFoundError, currentUser} from '@shockticketing/common';
+import { createChargeRouter} from "./routes/new";
 
 const app = express();
 app.set('trust proxy', true);
@@ -13,6 +14,8 @@ app.use(cookieSession({
 }));
 
 app.use(currentUser);
+
+app.use(createChargeRouter);
 
 app.all('*', async (req, res) => {
     throw new NotFoundError()
