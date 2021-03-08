759bab89ea2141f5a481468a1173ec52ae5ffb0f
shockliang <shockwater@gmail.com>
2021-03-09 00:05:03 +0800

    Automated payment testing.

--- payments/src/routes/__test__/new.test.ts
+++ payments/src/routes/__test__/new.test.ts
@@ -3,6 +3,9 @@ import mongoose from "mongoose";
 import {app} from "../../app";
 import {Order} from "../../models/order";
 import {OrderStatus} from "@shockticketing/common";
+import {stripe} from '../../stripe';
+
+jest.mock('../../stripe');
 
 it('return a 404 when purchasing an order that does not exist', async () => {
     await request(app)
@@ -54,4 +57,30 @@ it('return a 400 when purchasing a cancelled order', async () => {
             orderId: order.id
         })
         .expect(400);
+});
+
+it('returns a 201 with valid input', async () => {
+    const userId = mongoose.Types.ObjectId().toHexString();
+    const order = Order.build({
+        id: mongoose.Types.ObjectId().toHexString(),
+        userId,
+        version: 0,
+        price: 25,
+        status: OrderStatus.Created
+    });
+    await order.save();
+
+    await request(app)
+        .post('/api/payments')
+        .set('Cookie', global.signin(userId))
+        .send({
+            token: 'tok_visa',
+            orderId: order.id
+        })
+        .expect(201);
+
+    const chargeOption = (stripe.charges.create as jest.Mock).mock.calls[0][0];
+    expect(chargeOption.source).toEqual('tok_visa');
+    expect(chargeOption.amount).toEqual(25 * 100);
+    expect(chargeOption.currency).toEqual('usd');
 });
