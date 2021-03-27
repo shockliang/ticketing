69febe9417a04fedab87b54ca43c3f4f7bc94749
shockliang <shockwater@gmail.com>
2021-03-27 15:20:29 +0800

    Showing stripe payment form.

--- client/pages/orders/[orderId].js
+++ client/pages/orders/[orderId].js
@@ -1,6 +1,7 @@
 import {useEffect, useState} from 'react';
+import StripeCheckout from 'react-stripe-checkout';
 
-const OrderShow = ({order}) => {
+const OrderShow = ({order, currentUser}) => {
   const [timeLeft, setTimeLeft] = useState(0);
 
   useEffect(() => {
@@ -20,7 +21,17 @@ const OrderShow = ({order}) => {
   if (timeLeft < 0) {
     return <div>Order Expired</div>
   }
-  return <div>Time left to pay: {timeLeft} seconds</div>
+  return (
+    <div>
+      Time left to pay: {timeLeft} seconds
+      <StripeCheckout
+        token={(token) => console.log(token)}
+        stripeKey={"pk_test_AUY0kQhbYIepZnUkA5YWgFnL00JQH2uTdY"}
+        amount={order.ticket.price * 100}
+        email={currentUser.email}
+      />
+    </div>
+  )
 };
 
 OrderShow.getInitialProps = async (context, client) => {
